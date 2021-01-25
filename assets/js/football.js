9c60832c77294f31f02a87e4cc2b36f5a8fcfd72
Jeff Mead <jeffdmead171@gmail.com>
2021-01-25 16:10:48 -0500

    added startGame logic

--- assets/js/football.js
+++ assets/js/football.js
@@ -1,6 +1,38 @@
-console.log("Welcome to the superbowl");
+// Declare variables
+let startGameEl = document.querySelector('.kick-off');
+let errorEl = document.querySelector('.error-msg');
+let homeTeamEl = document.querySelector('#home-team');
+// let homeTeam = "";
+// let awayTeam = "";
+let awayTeamEl = document.querySelector('#away-team');
 
 
-function kickOff() {
 
+let startGame = function (homeTeam, awayTeam) {
+    homeTeamEl.innerHTML = "";
+    awayTeamEl.innerHTML = "";
+    let startGameEl = document.querySelector('.lets-go');
+    let startGM = document.createElement('p');
+    startGameEl.appendChild(startGM);
+    startGM.innerHTML = `Welcome to the start of the ${homeTeam} vs. ${awayTeam} football game.`;
+}
+
+
+// Get team names
+let getTeams = function (event) {
+    event.preventDefault();
+    errorEl.textContent = "";
+    let homeTeam = homeTeamEl.value.trim(); 
+    let awayTeam = awayTeamEl.value.trim(); 
+    if (!homeTeam || !awayTeam) {
+        errorEl.textContent = "Please enter a home and away team";
+    } else {
+        startGame(homeTeam, awayTeam);
+    }
+
+
+
+    console.log(event);
 };
+
+startGameEl.addEventListener("submit", getTeams);
